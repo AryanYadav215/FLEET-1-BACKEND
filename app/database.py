458723e4c38a758d19bfbddd95a42f1d211a2e5c
aditikeshri5 <aditi.keshri180706@gmail.com>
2026-03-19 19:19:04 +0530

    deployment ready2

--- app/database.py
+++ app/database.py
@@ -1,7 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres.wncueglfxgxzwzxkwcey:XiJt4Glwffqsi29M@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres?sslmode=require"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
